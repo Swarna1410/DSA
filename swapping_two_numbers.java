1e953fac01819199868a171cc53f337aac40dbb7
Swarna1410 <swarnagacharya14@gmail.com>
2025-11-05 11:36:24 +0530

    Create swapping_two_numbers.java

---
+++ swapping_two_numbers.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=10,b=20;
+	    int temp;
+	    temp=a;
+	    a=b;
+	    b=temp;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
