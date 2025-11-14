c2f2fae2d9c62434200f6ef5b6e1400c485f7ab8
Swarna1410 <swarnagacharya14@gmail.com>
2025-11-14 12:45:18 +0530

    Update sll_implemementaion.java

--- sll_implemementaion.java
+++ sll_implemementaion.java
@@ -1,7 +1,69 @@
 /******************************************************************************
 
                             implementation of ssl
-                            insertion,deletion,searchin
+                            insertion:
+                            
+                            
+                            public void insert(int data)
+{
+    Node hello=new Node(data);
+    if(head==null){
+        head=hello;
+        tail=hello;
+    }
+    else{
+        tail.next=hello;
+        tail=hello;
+    }
+}
+                            
+                            
+                            
+                            
+                            ,deletion,searching
+                            
+                            
+                            
+searching:
+
+void search(int val){
+    
+    int p=0;
+    Node n=head;
+    
+    while(n!=null){
+        if(n.data==val){
+            p=1;break;
+        }n=n.next;
+    }
+       if(p==1) 
+       System.out.println("found");
+       else
+    System.out.println("not found");
+    }
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 *******************************************************************************/
 class Node{
@@ -30,7 +92,39 @@ public void insert(int data)
         tail=hello;
     }
 }
-   public void disp(){
+
+
+void search(int val){
+    
+    int p=0;
+    Node n=head;
+    
+    while(n!=null){
+        if(n.data==val){
+            p=1;break;
+        }n=n.next;
+    }
+       if(p==1) 
+       System.out.println("found");
+       else
+    System.out.println("not found");
+    }
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+    void disp(){
       Node n=head;
       while(n!=null){
       System.out.print(n.data+" ->");
@@ -49,7 +143,7 @@ public class Main
 	    SLL o=new SLL();
 	    o.insert(10); o.insert(20); o.insert(30); o.insert(40);
 	    o.disp();
-	    
+	    o.search(70);
 		
 	}
 }
